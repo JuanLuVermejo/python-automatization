84446531f5e3d0b271b4086e08074decd0e32d1a
Juan Luis Vermejo Gonzales <juanvg75@ACIPROYECTOS.LOCAL>
2024-08-23 14:54:54 -0500

    Create file with old data added to Update Reports

--- Scripts/Update-Reports-New-Format/UpdateReports.py
+++ Scripts/Update-Reports-New-Format/UpdateReports.py
@@ -1,14 +1,12 @@
+import os
 import xlwings as xw
+import datetime
 
 def obtener_valores_y_rango(archivo_excel):
     try:
-        # Intentar abrir el archivo Excel
         wb = xw.Book(archivo_excel)
-        
-        # Seleccionar la hoja "Hoja de Calculos"
         hoja = wb.sheets['Hoja de Calculos']
 
-        # Inicializar variables para almacenar los resultados
         valor_total_consumo = None
         fila_item = None
         fila_total_m3h = None
@@ -19,13 +17,12 @@ def obtener_valores_y_rango(archivo_excel):
         ultimo_valor = None
         tiempo_trabajo = None
         dias_trabajo_por_mes = None
+        medidor_referencia_sap = None
 
-        # Recopilar el valor de la celda C2
         cuenta_contrato = hoja.range('C2').value
         print(f"Valor de 'N° Cuenta Contrato' (C2): {cuenta_contrato}")
 
-        # Buscar el valor en la columna B
-        for cell in hoja.range('B1:B1000'):  # Asumiendo que no hay más de 1000 filas
+        for cell in hoja.range('B1:B1000'):
             if cell.value in ["TOTAL CONSUMO RECUPERADO", "TOTAL CONSUMO RELIQUIDACIÓN"]:
                 valor_total_consumo = hoja.range(f'D{cell.row}').value
                 print(f"Valor de 'TOTAL CONSUMO': {valor_total_consumo}")
@@ -43,24 +40,26 @@ def obtener_valores_y_rango(archivo_excel):
                 dias_trabajo_por_mes = hoja.range(f'F{cell.row}').value
                 print(f"Valor de 'Días de Trabajo por Mes': {dias_trabajo_por_mes}")
 
-        # Validar que se hayan encontrado las filas necesarias
         if fila_item is None:
             raise ValueError("No se encontró 'Item' en la columna B.")
         if fila_total_m3h is None:
             raise ValueError("No se encontró 'Total m3/h' en la columna B.")
 
-        # Definir el rango basado en las filas encontradas
-        rango_celdas = hoja.range(f'C{fila_item + 1}:D{fila_total_m3h - 1}').value
+        rango_celdas = hoja.range(f'C{fila_item + 2}:D{fila_total_m3h - 1}').value
         print(f"Rango de celdas desde 'Item' hasta 'Total m3/h': {rango_celdas}")
 
-        # Validar si existe la hoja "BDDetalle"
+        for cell in hoja.range('B2:Z2'):
+            if cell.value == "Tipo de Calculo":
+                tipo_de_calculo = hoja.range(f'{cell.offset(0, 1).address}').value
+                print(f"Valor de 'Tipo de Calculo': {tipo_de_calculo}")
+            elif cell.value == "Medidor Referencia SAP":
+                medidor_referencia_sap = hoja.range(f'{cell.offset(0, 1).address}').value
+                print(f"Valor de 'Medidor Referencia SAP': {medidor_referencia_sap}")
+
         sheet_names = [sheet.name.strip() for sheet in wb.sheets]
-        
         if 'BDDetalle' in sheet_names:
             hoja_detalle = wb.sheets['BDDetalle']
-            
-            # Obtener el último valor no vacío en la columna A
-            valores_columna_a = hoja_detalle.range('A1:A1000').value  # Asumiendo que no hay más de 1000 filas
+            valores_columna_a = hoja_detalle.range('A1:A1000').value
             
             for valor in reversed(valores_columna_a):
                 if valor not in (None, '', 'Null'):
@@ -74,21 +73,62 @@ def obtener_valores_y_rango(archivo_excel):
     except Exception as e:
         print(f"Error: {e}")
     finally:
-        # Asegurarse de cerrar el archivo Excel si fue abierto correctamente
         try:
             wb.close()
         except UnboundLocalError:
             pass
 
-    # Retornar los valores recopilados
-    return (valor_total_consumo, rango_celdas, muestra_tipica_valores, 
-            tipo_de_calculo, cuenta_contrato, tiempo_trabajo, dias_trabajo_por_mes, ultimo_valor)
+    return (valor_total_consumo, rango_celdas, muestra_tipica_valores, tipo_de_calculo, cuenta_contrato, tiempo_trabajo, dias_trabajo_por_mes, ultimo_valor, medidor_referencia_sap)
 
-# Definir la ruta al archivo de Excel
+def insertar_datos_y_guardar(archivo_plantilla, cuenta_contrato, valor_total_consumo, rango_celdas, muestra_tipica_valores, tipo_de_calculo, ultimo_valor, tiempo_trabajo, dias_trabajo_por_mes, medidor_referencia_sap, archivo_original):
+    try:
+        wb_plantilla = xw.Book(archivo_plantilla)
+        hoja_plantilla = wb_plantilla.sheets['Hoja de Calculos']
+
+        # Convertir último valor a formato MM.YYYY
+        if isinstance(ultimo_valor, (datetime.datetime, datetime.date)):
+            ultimo_valor = ultimo_valor.strftime('%m.%Y')
+
+        hoja_plantilla.range('C2').value = cuenta_contrato
+        hoja_plantilla.range('A1').value = valor_total_consumo
+        hoja_plantilla.range('C10').value = rango_celdas
+        hoja_plantilla.range('C28').value = muestra_tipica_valores[0]
+        hoja_plantilla.range('D28').value = muestra_tipica_valores[1]
+        hoja_plantilla.range('T2').value = tipo_de_calculo
+        hoja_plantilla.range('W2').value = ultimo_valor
+        hoja_plantilla.range('F10').value = tiempo_trabajo
+        hoja_plantilla.range('F11').value = dias_trabajo_por_mes
+        hoja_plantilla.range('P2').value = medidor_referencia_sap
+
+        carpeta_original = os.path.dirname(archivo_original)
+        carpeta_actualizados = os.path.join(carpeta_original, 'Actualizados')
+        if not os.path.exists(carpeta_actualizados):
+            os.makedirs(carpeta_actualizados)
+
+        nombre_archivo_3 = f"{cuenta_contrato} -{tiempo_trabajo} - Informes CPNO.xlsx"
+        ruta_archivo_3 = os.path.join(carpeta_actualizados, nombre_archivo_3)
+
+        wb_plantilla.save(ruta_archivo_3)
+        print(f"Archivo guardado como: {ruta_archivo_3}")
+        
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        try:
+            wb_plantilla.close()
+        except UnboundLocalError:
+            pass
+
+# Ruta del archivo Excel original y de la plantilla
 archivo_excel = r'C:\Users\juan.vermejo\Documents\CPNO\Pruebas\Masivo\3035430 - 05.2024 - Informes CPNO.xlsx'
+archivo_plantilla = r"C:\Users\juan.vermejo\Documents\CPNO\Plantilla - Informes CPNO.xlsx"
 
-# Ejecutar la función y obtener los valores
+# Obtener los valores del archivo original
 (val_total_consumo, rango_celdas, muestra_tipica, tipo_calculo, cuenta_contrato, 
- tiempo_trabajo, dias_trabajo, ultimo_valor) = obtener_valores_y_rango(archivo_excel)
+ tiempo_trabajo, dias_trabajo, ultimo_valor, medidor_referencia_sap) = obtener_valores_y_rango(archivo_excel)
 
-
+# Insertar los valores en la plantilla y guardar el archivo actualizado
+insertar_datos_y_guardar(
+    archivo_plantilla, cuenta_contrato, val_total_consumo, rango_celdas, muestra_tipica, 
+    tipo_calculo, ultimo_valor, tiempo_trabajo, dias_trabajo, medidor_referencia_sap, archivo_excel
+)
